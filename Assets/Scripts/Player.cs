a6cd119a50720fa21f00aad6c0d1daf19e08e682
Sammy Lizon <lizon_sammy@yahoo.co.uk>
2023-04-01 00:27:32 +0100

    Adding first person camera in player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
 {
     public float speed = 23.75f;
     public CharacterController myController;
+    public Transform myCameraHead;
+
+    private float mouseSensitivity = 850f;
+    private float cameraVerticalRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +21,9 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
-        
+        CameraMovement();
+
+
     }
 
     void Movement()
@@ -31,5 +37,21 @@ public class Player : MonoBehaviour
 
         //Make the player move and multiply by Speed and deltaTime to make it smooth across systems
         myController.Move(move * speed * Time.deltaTime);
+    }
+
+    void CameraMovement()
+    {
+        //Get mouse x and y input anf multiply it by sensitivity and time delta 
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //invert because Unity registers looking down as postive and looking up as negative whereas up is positive.
+        cameraVerticalRotation -= mouseY;
+
+        //Rotate the camera left and rigth 
+        transform.Rotate(Vector3.up * mouseX);
+
+        //Rotate the camera up and down independent of the body 
+        myCameraHead.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
     }
 }
