54eb30b92fbf84b744757666c54cc2c61c786eb4
Sammy Lizon <lizon_sammy@yahoo.co.uk>
2023-03-31 23:22:39 +0100

    Added basic movement, using CharacterController.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 0.1f;
+    public CharacterController myController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = Vector3.forward * y + Vector3.right * x;
+
+        myController.Move(move);  
     }
 }
